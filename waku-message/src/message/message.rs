21741667cfee02d3ad7de3c885514f711ebae95f
Lorenzo Delgado <lnsdev@proton.me>
2022-12-23 19:10:44 +0100

    feat(waku-message): add custom debug trait implementation to waku message (#30)

--- waku-message/src/message/message.rs
+++ waku-message/src/message/message.rs
@@ -1,12 +1,30 @@
+use std::fmt::{Debug, Formatter};
+
 use bytes::Bytes;
 
 use crate::content_topic::ContentTopic;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct WakuMessage {
     pub payload: Bytes,
     pub content_topic: ContentTopic,
     pub version: u32,
     pub timestamp: Option<i64>,
     pub ephemeral: bool,
+}
+
+impl Debug for WakuMessage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let payload_slice = self.payload.get(0..32).unwrap_or(&self.payload[..]);
+        f.debug_struct("WakuMessage")
+            .field("content_topic", &self.content_topic)
+            .field("version", &self.version)
+            .field("timestamp", &self.timestamp)
+            .field("ephemeral", &self.ephemeral)
+            .field(
+                "payload",
+                &format_args!("Bytes(0x{}…)", hex::encode(payload_slice)),
+            )
+            .finish()
+    }
 }
