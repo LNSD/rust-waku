8ac79dc7180df2ae640b2640cddf264a33ef4da0
Lorenzo Delgado <lnsdev@proton.me>
2023-05-06 01:55:48 +0200

    feat(waku-core): added namespaced pub-sub topics

---
+++ waku-core/src/pubsub_topic/topic.rs
@@ -0,0 +1,107 @@
+///! Waku pubsub topic.
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
+pub struct PubsubTopic(String);
+
+impl PubsubTopic {
+    pub fn new<S>(topic: S) -> PubsubTopic
+        where
+            S: Into<String>,
+    {
+        PubsubTopic(topic.into())
+    }
+
+    /// Return the length in bytes of this content topic.
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Returns true if the length of this content topic.
+    pub fn is_empty(&self) -> bool {
+        self.0.len() == 0
+    }
+
+    /// Return a copy of this conte topic's byte representation.
+    pub fn to_vec(&self) -> Vec<u8> {
+        self.0.clone().into_bytes()
+    }
+}
+
+impl fmt::Debug for PubsubTopic {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.to_string().fmt(f)
+    }
+}
+
+impl fmt::Display for PubsubTopic {
+    /// Convert a PubsubTopic to a string
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl FromStr for PubsubTopic {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.to_string()))
+    }
+}
+
+impl From<String> for PubsubTopic {
+    fn from(s: String) -> Self {
+        Self(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pubsub_topic_new_from_str() {
+        // Given
+        let topic_str = "test";
+
+        // When
+        let topic = PubsubTopic::new(topic_str);
+
+        // Then
+        assert_eq!(topic.to_string(), "test");
+        assert_eq!(topic.len(), 4);
+        assert!(!topic.is_empty());
+        assert_eq!(topic.to_vec(), vec![116, 101, 115, 116]);
+    }
+
+    #[test]
+    fn test_pubsub_topic_from_str() {
+        // Given
+        let topic_str = "test";
+
+        // When
+        let topic = PubsubTopic::from_str(topic_str).unwrap();
+
+        // Then
+        assert_eq!(topic.to_string(), "test");
+        assert_eq!(topic.len(), 4);
+        assert!(!topic.is_empty());
+        assert_eq!(topic.to_vec(), vec![116, 101, 115, 116]);
+    }
+
+    #[test]
+    fn test_pubsub_topic_from_string() {
+        // Given
+        let topic_str = "test".to_string();
+
+        // When
+        let topic = PubsubTopic::from(topic_str);
+
+        // Then
+        assert_eq!(topic.to_string(), "test");
+        assert_eq!(topic.len(), 4);
+        assert!(!topic.is_empty());
+        assert_eq!(topic.to_vec(), vec![116, 101, 115, 116]);
+    }
+}
