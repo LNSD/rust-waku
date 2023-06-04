822e3a40270bc6f33a5f3c820cae1b5d8110c07e
Lorenzo Delgado <lnsdev@proton.me>
2023-06-04 13:48:41 +0200

    test(waku-relay): added gossipsub smoke integration test (#86)

---
+++ waku-relay/tests/testlib/transport.rs
@@ -0,0 +1,24 @@
+use std::time::Duration;
+
+use libp2p::core::muxing::StreamMuxerBox;
+use libp2p::core::transport::{Boxed, MemoryTransport, Transport};
+use libp2p::core::upgrade::Version;
+use libp2p::identity::{Keypair, PeerId};
+use libp2p::{noise, yamux};
+
+/// Type alias for libp2p transport
+pub type P2PTransport = (PeerId, StreamMuxerBox);
+/// Type alias for boxed libp2p transport
+pub type BoxedP2PTransport = Boxed<P2PTransport>;
+
+/// In memory transport
+pub fn test_transport(keypair: &Keypair) -> std::io::Result<BoxedP2PTransport> {
+    let transport = MemoryTransport::default();
+
+    Ok(transport
+        .upgrade(Version::V1)
+        .authenticate(noise::Config::new(keypair).unwrap())
+        .multiplex(yamux::Config::default())
+        .timeout(Duration::from_secs(20))
+        .boxed())
+}
